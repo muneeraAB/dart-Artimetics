7c60172c30b4d224b9231d82192d73cce68e36ea
muneeraAB <112892655+muneeraAB@users.noreply.github.com>
2024-09-30 10:16:45 +0300

    initial commit

---
+++ main.dart
@@ -0,0 +1,7 @@
+void main() {
+  double tempInFahrenheit = 86;
+  //double celsius = 10;
+  double tempInCelsius = (tempInFahrenheit - 32) / 1.8;
+  print("$tempInFahrenheit F = $tempInCelsius C");
+  print("$tempInCelsius C = $tempInFahrenheit F");
+}
